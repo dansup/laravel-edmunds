512fd3f6be691284ada0d8d4f4dc84494e4b09fb
Daniel Supernault <danielsupernault@gmail.com>
2016-05-25 21:23:04 -0600

    Add missing response formats
    Allow array, object, stream and collection API response.

--- src/Two/Client.php
+++ src/Two/Client.php
@@ -43,22 +43,25 @@ abstract class Client {
     $body = $this->formatResponse($response, $format);
     return $body;
   }
-  public function formatResponse($response, $format)
+  public function formatResponse($response, $format = "array")
   {
     $body = null;
     switch ($format) {
       case 'array':
+      $body = json_decode($response->getBody(), true);
+      break;
+      case 'object':
       $body = json_decode($response->getBody());
       break;
       case 'stream':
       $body = $response->getBody();
       break;
       case 'collection':
-      $body = collect(json_decode($response->getBody()));
+      $body = collect(json_decode($response->getBody(), true));
       break;
       
       default:
-      $body = json_decode($response->getBody());
+      $body = json_decode($response->getBody(), true);
       break;
     }
     return $body;
