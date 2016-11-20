b8c63d088ddc0ee3de35d6b5618f43d0a0a429af
Daniel Supernault <danielsupernault@gmail.com>
2016-11-20 16:35:14 -0700

    Remove RequestException in Guzzle
    Pass request errors as JSON rather than a RequestException.

--- src/Two/Client.php
+++ src/Two/Client.php
@@ -4,66 +4,72 @@ namespace Dansup\Edmunds\Two;
 
 use GuzzleHttp;
 
-abstract class Client {
+abstract class Client
+{
+    public $client;
+    public $apiUrl;
 
-  public $client;
-  public $apiUrl;
-
-  public function __construct()
-  {
-    $client = new GuzzleHttp\Client([
-      'query' => [
-      'api_key' => config('laravel-edmunds.api_key')
-      ]
+    public function __construct()
+    {
+      $client = new GuzzleHttp\Client([
+        'query' => [
+          'api_key' => config('laravel-edmunds.api_key'),
+        ],
       ]);
-    $this->client = $client;
-    $this->apiUrl = 'https://api.edmunds.com/api';
-  }
-
-  public function client()
-  {
-    return $this->client;
-  }
-  public function apiUrl()
-  {
-    return $this->apiUrl;
-  }
-  public function queryString($endpoint, $queryKey="null", $queryValue="null", $format="array")
-  {
-    $client = new GuzzleHttp\Client();
-    $apiKey = config('laravel-edmunds.api_key');
-    $queryString = "?{$queryKey}={$queryValue}&api_key={$apiKey}";
-    $response = $client->request('GET', $endpoint.$queryString, ['stream' => false]);
-    $body = $this->formatResponse($response, $format);
-    return $body;
-  }
-  public function query($endpoint, $format = "array")
-  {
-    $response = $this->client->request('GET', $endpoint, ['stream' => false]);
-    $body = $this->formatResponse($response, $format);
-    return $body;
-  }
-  public function formatResponse($response, $format = "array")
-  {
-    $body = null;
-    switch ($format) {
-      case 'array':
-      $body = json_decode($response->getBody(), true);
-      break;
-      case 'object':
-      $body = json_decode($response->getBody());
-      break;
-      case 'stream':
-      $body = $response->getBody();
-      break;
-      case 'collection':
-      $body = collect(json_decode($response->getBody(), true));
-      break;
-      
-      default:
-      $body = json_decode($response->getBody(), true);
-      break;
+      $this->client = $client;
+      $this->apiUrl = 'https://api.edmunds.com/api';
     }
-    return $body;
-  }
+
+    public function client()
+    {
+        return $this->client;
+    }
+
+    public function apiUrl()
+    {
+        return $this->apiUrl;
+    }
+
+    public function queryString($endpoint, $queryKey = 'null', $queryValue = 'null', $format = 'array')
+    {
+        $client = new GuzzleHttp\Client();
+        $apiKey = config('laravel-edmunds.api_key');
+        $queryString = "?{$queryKey}={$queryValue}&api_key={$apiKey}";
+        $response = $client->request('GET', $endpoint.$queryString, ['stream' => false, 'http_errors' => false]);
+        $body = $this->formatResponse($response, $format);
+
+        return $body;
+    }
+    public function query($endpoint, $format = 'array')
+    {
+        $response = $this->client->request('GET', $endpoint, ['stream' => false, 'http_errors' => false]);
+        $body = $this->formatResponse($response, $format);
+
+        return $body;
+    }
+
+    public function formatResponse($response, $format = 'array')
+    {
+        $body = null;
+        switch ($format) {
+        case 'array':
+        $body = json_decode($response->getBody(), true);
+        break;
+        case 'object':
+        $body = json_decode($response->getBody());
+        break;
+        case 'stream':
+        $body = $response->getBody();
+        break;
+        case 'collection':
+        $body = collect(json_decode($response->getBody(), true));
+        break;
+
+        default:
+        $body = json_decode($response->getBody(), true);
+        break;
+    }
+
+        return $body;
+    }
 }
